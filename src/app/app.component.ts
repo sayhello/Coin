cdaefb927c7009d9857eb29c05eb0f98906c3bc0
sayhello <gsaiwe@safe-mail.net>
2017-09-21 10:29:31 +0200

    hacking
    hacking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,28 @@ export class AppComponent {
   priceStart;
 
   //ETH
-  ethereumCoin = {};
+  ethereumCoin = {
+    name: 'ETH',
+    price: null,
+    dataBag: [],
+    difference: null,
+    priceStart: null,
+    priceEnd: null,
+    differencePercentage: null
+  };
+
+   //BTC
+   bitcoinCoin = {
+    name: 'BTC',
+    price: null,
+    dataBag: [],
+    difference: null,
+    priceStart: null,
+    priceEnd: null,
+    differencePercentage: null
+  }; 
+
+  amount = 30;
 
 
   constructor() {
@@ -39,16 +60,42 @@ export class AppComponent {
     this.socket = io.connect('http://socket.coincap.io');
 
 
-    /*this.socket.on('/coins', (globalMsg) => {
-      console.log(globalMsg);
-  })*/
-
     this.socket.on('trades', (tradeMsg) => {
 
       //console.log(tradeMsg);
       //this.price = tradeMsg.message.msg.price;
       //this.coin = tradeMsg.message.msg.long;
-      if (tradeMsg.message.coin == "ETH") {
+
+      // ETH
+      if (tradeMsg.message.coin == this.ethereumCoin.name) {
+
+        this.makeCoin(this.ethereumCoin, this.amount, tradeMsg);
+
+        //this.ethereumCoin.price = tradeMsg.message.msg.price;
+
+        // add to data bag
+        //if(this.ethereumCoin.dataBag.length <= this.amount){
+        //  this.ethereumCoin.dataBag.push(tradeMsg.message.msg.price);
+        //}
+
+        // calculate
+        //if(this.ethereumCoin.dataBag.length == this.amount){
+        //  this.ethereumCoin.difference = parseFloat(this.ethereumCoin.dataBag[this.amount-1]) - parseFloat(this.ethereumCoin.dataBag[0]);
+        //  this.ethereumCoin.priceStart =  parseFloat(this.ethereumCoin.dataBag[0]);
+        //  this.ethereumCoin.priceEnd =  parseFloat(this.ethereumCoin.dataBag[this.amount-1]);
+        //  this.ethereumCoin.differencePercentage = (this.ethereumCoin.priceEnd-this.ethereumCoin.priceStart)/this.ethereumCoin.priceStart * 100
+        //  this.ethereumCoin.dataBag.length = 0;
+        // }
+
+      }
+
+      //BTC
+      if (tradeMsg.message.coin == this.bitcoinCoin.name) {
+        this.makeCoin(this.bitcoinCoin, this.amount, tradeMsg);
+      }
+
+
+      /*if (tradeMsg.message.coin == "ETH") {
         this.coin = tradeMsg.message.msg.long;
         this.price = tradeMsg.message.msg.price;
         
@@ -83,16 +130,30 @@ export class AppComponent {
         }
 
 
-      }
+      }*/
     })
 
   }
 
 
-  ethereumTrading(){
+  makeCoin(coin, amount, lifeData) {
+
+    coin.price = lifeData.message.msg.price;
+
+    // add to data bag
+    if (coin.dataBag.length <= amount) {
+      coin.dataBag.push(lifeData.message.msg.price);
+    }
+
+    if (coin.dataBag.length == amount) {
+      coin.difference = parseFloat(coin.dataBag[amount - 1]) - parseFloat(coin.dataBag[0]);
+      coin.priceStart = parseFloat(coin.dataBag[0]);
+      coin.priceEnd = parseFloat(coin.dataBag[amount - 1]);
+      coin.differencePercentage = (coin.priceEnd - coin.priceStart) / coin.priceStart * 100
+      coin.dataBag.length = 0;
+    }
 
   }
-
 
 
 }
